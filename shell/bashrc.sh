69f33a8939941ef38b66d3507f30d666e8b1254e
ucizi <ucizim@gmail.com>
2020-03-13 00:34:59 +0000

    pureline

--- shell/bashrc.sh
+++ shell/bashrc.sh
@@ -151,3 +151,7 @@ export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
 export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
 export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
 export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+
+if [ "$TERM" != "linux" ]; then
+    source ~/configs2.0/pureline/pureline ~/configs2.0/pureline.conf
+fi
