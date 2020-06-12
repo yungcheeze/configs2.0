fe20937bdde1d6ed6e398877739e9678875e6c08
Ucizi Mafeni <ucizim@gmail.com>
2020-06-12 16:46:45 +0100

    lazygit alias

--- shell/bashrc.sh
+++ shell/bashrc.sh
@@ -43,6 +43,9 @@ export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
 export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
 export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
 
+#lazygit config
+alias lg=lazygit
+
 # HSTR configuration - add this to ~/.bashrc
 alias hh=hstr                    # hh to be alias for hstr
 export HSTR_CONFIG=hicolor       # get more colors
